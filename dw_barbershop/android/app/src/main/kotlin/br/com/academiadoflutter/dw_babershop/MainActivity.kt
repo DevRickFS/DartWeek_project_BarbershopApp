e925b79e3c0afcf062bfc582cdc513ccf14fe049
DevRickFS <devrickhc@gmail.com>
2023-08-19 15:39:36 -0300

    Commmit inicial projeto DartWeek 11 do Rodrigo Rahman

---
+++ dw_barbershop/android/app/src/main/kotlin/br/com/academiadoflutter/dw_babershop/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.academiadoflutter.dw_babershop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dw_barbershop/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:dw_babershop/src/barbershop_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BarbershopApp());
+}
+
