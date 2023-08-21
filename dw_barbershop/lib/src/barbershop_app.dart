6aa0b5ed7c49e0f89d297b7f58747f2f73297d8f
DevRickFS <devrickhc@gmail.com>
2023-08-21 14:30:37 -0300

    implementação da splash Page

--- dw_barbershop/lib/src/barbershop_app.dart
+++ dw_barbershop/lib/src/barbershop_app.dart
@@ -1,17 +1,26 @@
+
+import 'package:asyncstate/widget/async_state_builder.dart';
 import 'package:dw_babershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
+
+import 'core/ui/widgets/barbershop_loader.dart';
 
 class BarbershopApp extends StatelessWidget {
   const BarbershopApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'DW Barbershop',
-      routes: {
-        '/': (_) => const SplashPage()
-      },
-    );
+    return AsyncStateBuilder(
+      customLoader: const BarbershopLoader(),
+      builder: (asyncNavigatorObserver){
+        return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        navigatorObservers: [asyncNavigatorObserver],
+        title: 'DW Barbershop',
+        routes: {
+          '/': (_) => const SplashPage()
+        },
+      );
+    });
   }
 }
